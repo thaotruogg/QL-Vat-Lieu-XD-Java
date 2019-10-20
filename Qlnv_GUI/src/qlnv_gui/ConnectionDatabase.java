99360356ca44ae59010c5b7c3262fcd91fedc2a2
Truong Minh Thao <43067475+thaotruogg@users.noreply.github.com>
2019-10-20 11:14:07 +0700

    asdk

--- Qlnv_GUI/src/qlnv_gui/ConnectionDatabase.java
+++ Qlnv_GUI/src/qlnv_gui/ConnectionDatabase.java
@@ -16,23 +16,48 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
  
 public class ConnectionDatabase {
-    private static String DB_URL = "jdbc:sqlserver://localhost:1433;\"\n" + " + \"databaseName=DatabaseVLXD;" + "user=sa;" + "password=123456;";
+    //private static String DB_URL = "jdbc:sqlserver://localhost:1433;\"\n" + " + \"databaseName=DatabaseVLXD;" + "user=sa;" + "password=123456;";
+    private static String DB_URL = "jdbc:sqlserver://localhost:1433;"
+            + "databaseName=DatabaseVLXD;"
+            + "integratedSecurity=true";
     private static String USER_NAME = "sa";
     private static String PASSWORD = "123456";
     
-    public static Connection getConnection(){
+    
+    public static Connection getConnection(String dbURL, String userName, String password) {
+        Connection conn = null;
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             try {
-                return DriverManager.getConnection(DB_URL);
-            } catch (SQLException e) {
-                Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, e);
-            }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, ex);
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                conn = DriverManager.getConnection(dbURL, userName, password);
+                System.out.println("connect successfully!");
+                JOptionPane.showMessageDialog(null, "Connect successfully!");
+            } catch (SQLException ex) {
+                System.out.println("connect failure!");
+                JOptionPane.showMessageDialog(null, "Connect failure!");
+                Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+        return conn;
     }
+    
+//    public static Connection getConnection(){
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            try {
+//                return DriverManager.getConnection(DB_URL);
+//            } catch (SQLException e) {
+//                Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, e);
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(ConnectionDatabase.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return null;
+//    }
 }
